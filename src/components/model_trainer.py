945356da6575e114b34238e33c7690f0fe6a3e80
garvagrawal9084 <garvagrawal9084@gmail.com>
2026-08-24 17:36:00 +0530

    hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,13 +47,88 @@ class ModelTrainer :
                 "Random Forest" : RandomForestRegressor() ,
                 "Decision Tree" : DecisionTreeRegressor() ,
                 "Gradient Boosting" : GradientBoostingRegressor() ,
-                "CatBoosting Classification" : CatBoostRegressor(verbose=False) ,
+                "CatBoosting Regression" : CatBoostRegressor(verbose=False) ,
                 "AdaBoost Regression" : AdaBoostRegressor() ,
                 "XGBRegression" : XGBRegressor()
             }
 
+            params = {
+
+                "Linear Regression": {
+                    "fit_intercept": [True, False],
+                    "positive": [True, False]
+                },
+
+                "Ridge": {
+                    "alpha": [0.001, 0.01, 0.1, 1, 10, 100],
+                    "fit_intercept": [True, False],
+                    "solver": ["auto", "svd", "cholesky", "lsqr", "sag", "saga"]
+                },
+
+                "Lasso": {
+                    "alpha": [0.0001, 0.001, 0.01, 0.1, 1, 10],
+                    "fit_intercept": [True, False],
+                    "selection": ["cyclic", "random"]
+                },
+
+                "K-Neighbour Regression": {
+                    "n_neighbors": [3, 5, 7, 10, 15, 20],
+                    "weights": ["uniform", "distance"],
+                    "algorithm": ["auto", "ball_tree", "kd_tree", "brute"],
+                    "p": [1, 2]
+                },
+
+                "Random Forest": {
+                    "n_estimators": [100, 200, 300],
+                    "max_depth": [None, 5, 10, 20, 30],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4],
+                    "max_features": ["sqrt", "log2", None]
+                },
+
+                "Decision Tree": {
+                    "criterion": ["squared_error", "friedman_mse", "absolute_error"],
+                    "max_depth": [None, 5, 10, 20, 30],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4],
+                    "max_features": ["sqrt", "log2", None]
+                },
+
+                "Gradient Boosting": {
+                    "n_estimators": [100, 200, 300],
+                    "learning_rate": [0.01, 0.05, 0.1, 0.2],
+                    "max_depth": [3, 5, 7],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4],
+                    "subsample": [0.8, 0.9, 1.0]
+                },
+
+                "CatBoosting Regression": {
+                    "iterations": [100, 200, 500],
+                    "depth": [4, 6, 8, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "l2_leaf_reg": [1, 3, 5, 10]
+                },
+
+                "AdaBoost Regression": {
+                    "n_estimators": [50, 100, 200, 300],
+                    "learning_rate": [0.01, 0.05, 0.1, 0.5, 1.0],
+                    "loss": ["linear", "square", "exponential"]
+                },
+
+                "XGBRegression": {
+                    "n_estimators": [100, 200, 300],
+                    "max_depth": [3, 5, 7, 10],
+                    "learning_rate": [0.01, 0.05, 0.1, 0.2],
+                    "subsample": [0.7, 0.8, 0.9, 1.0],
+                    "colsample_bytree": [0.7, 0.8, 0.9, 1.0],
+                    "min_child_weight": [1, 3, 5],
+                    "gamma": [0, 0.1, 0.2]
+                }
+            }
+
             model_report : dict = evaluate_models(x_train = X_train , y_train = y_train
-                                            , x_test= X_test , y_test = y_test , models = models)
+                                            , x_test= X_test , y_test = y_test , models = models , params = params)
 
             best_model_score = max(sorted(model_report.values()))
 
--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,11 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from scipy.special.cython_special import cbrt
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
+from sklearn.model_selection import RandomizedSearchCV
 
 
 def save_object(file_path , obj) :
@@ -22,18 +24,21 @@ def save_object(file_path , obj) :
         raise CustomException(e , sys)
 
 
-def evaluate_models(x_train , y_train  , x_test , y_test , models:dict) :
+def evaluate_models(x_train , y_train  , x_test , y_test , models:dict , params : dict) :
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
 
-            model.fit(x_train , y_train)
+            rs = RandomizedSearchCV(model , para , cv=5 , n_jobs=-1 , verbose=3 , refit=True)
+            rs.fit(x_train, y_train)
 
-            y_train_pred = model.predict(x_train)
+            best_model = rs.best_estimator_
 
-            y_test_pred = model.predict(x_test)
+            y_train_pred = best_model.predict(x_train)
+            y_test_pred = best_model.predict(x_test)
 
             train_model_score = r2_score(y_train , y_train_pred)
             test_model_score = r2_score(y_test , y_test_pred)
