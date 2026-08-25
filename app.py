8f57ebe58f09d69ae9b4afd722416db8bffbc42c
garvagrawal9084 <garvagrawal9084@gmail.com>
2026-08-25 22:59:16 +0530

    final

--- app.py
+++ app.py
@@ -1,16 +1,4 @@
-import sys
-import sklearn
 import streamlit as st
-from sklearn.pipeline import Pipeline
-
-st.write("Python:", sys.version)
-st.write("Scikit-learn:", sklearn.__version__)
-st.write(
-    "Pipeline has transform_input:",
-    hasattr(Pipeline, "transform_input")
-)
-
-st.stop()
 
 from src.pipelines.predict_pipeline import CustomData, PredictPipelines
 
